33190e34cf29cd234e2805e4fe8d685e4eca1383
RaymondZziwa <raymondzian@gmail.com>
2024-08-30 17:39:47 +0300

    worked on auth

--- user-auth/src/cache/config/redis.module.ts
+++ user-auth/src/cache/config/redis.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
-import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-yet';
+//import { CacheModule } from '@nestjs/cache-manager';
+//import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
-    CacheModule.register({
-      store: redisStore,
-      url: 'redis://181.215.68.133:6379',
-      ttl: 1800000,
-      legacyMode: true,
-    }),
+    // CacheModule.register({
+    //   store: redisStore,
+    //   url: 'redis://181.215.68.133:6379',
+    //   ttl: 1800000,
+    //   legacyMode: true,
+    // }),
   ],
   providers: [RedisService],
   exports: [RedisService],
